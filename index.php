65cff29936bf3217b1df2f5a997d1527accb4b8d
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-03 09:04:43 +0100

    Fixed addon download URL fetching due to API changes

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 
 			header("Pragma: no-cache");
 			header("Expires: 0");
-			header("Location: " . $result["url"]);
+			header("Location: " . $result["data"]["url"]);
 			header("Cache-Control: no-store, no-cache, must-revalidate");
 			header("Cache-Control: post-check=0, pre-check=0", false);
 
