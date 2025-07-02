24c82fe02e0576f15a257e6440b8fb27cce75016
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-07-02 17:22:36 +0200

    Removed legacy CSS stylesheets from PHP version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import "./app.css";
-import { mount } from "svelte";
 import App from "./App.svelte";
+import { mount } from "svelte";
 
 const app = mount( App, {
 	target: document.querySelector( "body" ) as Element
